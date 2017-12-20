f92e7caeee77c17252e82e5eac60ef91626e2236
Emory Merryman <emory.merryman@gmail.com>
2017-12-20 11:44:21 +0000

    do not use command line arguments ... everything comes in through environment variables

--- root/init.user.sh
+++ root/init.user.sh
@@ -1,76 +1,15 @@
 #!/bin/sh
 
-while [ ${#} -gt 0 ]
-do
-    case ${1} in
-        --gpg-secret-key)
-            TEMP=$(mktemp) &&
-                echo "${2}" > ${TEMP} &&
-                gpg --batch --import ${TEMP} &&
-                rm -f ${TEMP} &&
-                shift 2
-        ;;
-        --gpg2-secret-key)
-            TEMP=$(mktemp) &&
-                echo "${2}" > ${TEMP} &&
-                gpg2 --batch --import ${TEMP} &&
-                rm -f ${TEMP} &&
-                shift 2
-        ;;
-        --gpg-owner-trust)
-            TEMP=$(mktemp) &&
-                echo "${2}" > ${TEMP} &&
-                gpg --batch --import-ownertrust ${TEMP} &&
-                rm -f ${TEMP} &&
-                shift 2
-        ;;
-        --gpg2-owner-trust)
-            TEMP=$(mktemp) &&
-                echo "${2}" > ${TEMP} &&
-                gpg2 --batch --import-ownertrust ${TEMP} &&
-                rm -f ${TEMP} &&
-                shift 2
-        ;;
-        --gpg-key-id)
-            GPG_KEY_ID="${2}" &&
-                shift 2
-        ;;
-        --user-name)
-            USER_NAME="${2}" &&
-                shift 2
-        ;;
-        --user-email)
-            USER_EMAIL="${2}" &&
-                shift 2
-        ;;
-        --origin-id-rsa)
-            echo "${2}" > /home/user/.ssh/origin_id_rsa &&
-                shift 2
-        ;;
-        --origin-organization)
-            ORIGIN_ORGANIZATION="${2}" &&
-                shift 2
-        ;;
-        --origin-repository)
-            ORIGIN_REPOSITORY="${2}" &&
-                shift 2
-        ;;
-        --host-name)
-            HOST_NAME="${2}" &&
-                shift 2
-        ;;
-        --host-port)
-            HOST_PORT="${2}" &&
-                shift 2
-        ;;
-        *)
-            echo Unknown Option &&
-                echo ${0} &&
-                echo ${@} &&
-                exit 64
-        ;;
-    esac
-done &&
+TEMP=$(mktemp -d) &&
+    echo "${GPG_SECRET_KEY}" > ${TEMP}/gpg-secret-key &&
+    gpg --batch --import ${TEMP}/gpg-secret-key &&
+    echo "${GPG2_SECRET_KEY}" > ${TEMP}/gpg2-secret-key &&
+    gpg2 --batch --import ${TEMP}/gpg2-secret-key &&
+    echo "${GPG_OWNER_TRUST}" > ${TEMP}/gpg-owner-trust &&
+    gpg --batch --import-ownertrust ${TEMP}/gpg-owner-trust &&
+    echo "${GPG2_OWNER_TRUST}" > ${TEMP}/gpg2-owner-trust &&
+    gpg2 --batch --import-ownertrust ${TEMP}/gpg2-owner-trust &&
+    rm -rf ${TEMP} &&
     pass init ${GPG_KEY_ID} &&
     pass git init &&
     pass git config user.name "${USER_NAME}" &&
