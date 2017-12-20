d54c40e119c33fcdc1d458afb610c7802a039fdf
Emory Merryman <emory.merryman@gmail.com>
2017-12-20 18:57:25 +0000

    add completion

--- root/run.root.sh
+++ root/run.root.sh
@@ -5,5 +5,6 @@ dnf update --assumeyes &&
     dnf install --assumeyes gnupg && 
     dnf install --assumeyes pass &&
     dnf install --assumeyes findutils &&
+    dnf install --assumeyes bash-completion &&
     dnf update --assumeyes &&
     dnf clean all
